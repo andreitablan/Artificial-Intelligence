2eb6885ccb45f6e3adb953780aefb507a823a380
Andrie Tablan <andreitablan01@gmail.com>
2022-10-25 21:18:23 +0300

    .

--- Laborator 4/main.py
+++ Laborator 4/main.py
@@ -14,11 +14,25 @@ def place_queen(aux_states, size, x, y):
                 if states2[i][j] != 1:
                     if x != i and j != y:
                         if x - y == i - j or x + y == i + j:
-                            states2[i][j] = -1
+                            if states2[i][j] != 2:
+                                states2[i][j] -= 1
                     if i == x or j == y:
-                        states2[i][j] = -1
+                        if states2[i][j] != 2:
+                            states2[i][j] -= 1
+    return states2
 
 
+def remove_queen(aux_states, size, x, y):
+    states2 = aux_states.copy()
+    states2[x][y] = 2
+    for i in range(0, size):
+        for j in range(0, size):
+            if states2[i][j] < 0:
+                if x != i and j != y:
+                    if x - y == i - j or x + y == i + j:
+                        states2[i][j] += 1
+                if i == x or j == y:
+                    states2[i][j] += 1
     return states2
 
 
@@ -33,74 +47,75 @@ def is_domain_empty(states, size, queen):
             return False
     return True
 
+def is_domain_final(states, size, queen):
+    for j in range(0, size):
+        if states[queen][j] == 1:
+            return True
+    return False
+
 
 def select_value_forward_checking(size, aux_states, states, queen):
     while not is_domain_empty(aux_states, size, queen):
 
         print("o noua iteratie")
-
-
-        #for x in range(0, size):
-         #   if aux_states[queen][x] == 1:
-          #      aux_states[queen][x] = -1
-
         for j in range(0, size):
             if aux_states[queen][j] == 0:
                 position = j
                 break
-
-        aux_states=copy.deepcopy(place_queen(aux_states, size, queen, position))
+        aux_states = copy.deepcopy(place_queen(aux_states, size, queen, position))
 
         print("----------------")
         print_states(aux_states, size)
 
-        empty_domain = False
-
-        for k in range(queen + 1, size):
+        k=queen+1
+        while k<size:
+            for j in range(0, size):
+                if aux_states[queen][j] == 0:
+                    position = j
+                    break
+            placed = 0
             for b in range(0, size):
                 if aux_states[k][b] == 0:
-                    aux_states = place_queen(aux_states, size, k, b)
+                    aux_states = copy.deepcopy(place_queen(aux_states, size, k, b))
                     print("--")
                     print_states(aux_states, size)
+                    placed = 1
                     break
-
-            empty_domain = True
-            for b in range(0, size):
-                if aux_states[k][b] != -1:
-                    empty_domain = False
-                    break
-            if empty_domain:
-                break
-
-        if empty_domain:
-
-            for k in range(queen + 1, size):
-                aux_states[k] = states[k].copy()
-
-            print("am resetat:")
-            print_states(aux_states, size)
-
-        else:
-            return position
+            if placed == 0:
+                for b in range(0, size):
+                    if aux_states[k - 1][b] == 1:
+                        aux_states = copy.deepcopy(remove_queen(aux_states, size, k - 1, b))
+                k -= 1
+                print("am resetat:")
+                print_states(aux_states, size)
+            else:
+                k+=1
+        ok = True
+    for queen in range(0, size):
+        if not is_domain_final(aux_states, size, queen):
+            ok = False
+            break
+    if ok is True:
+        return position
     print("am returnat none")
     return None
 
 
 def generalized_lookahead(size, states):
-    aux_states = copy.deepcopy(states)
     index = 0
     while size > index >= 0:
+        aux_states=copy.deepcopy(states)
         j = select_value_forward_checking(size, aux_states, states, index)
         if j is None:
             index -= 1
             for index1 in range(index + 1, size):
                 aux_states[index1] = states[index1]
         else:
-            print("am primit pozitia ", index, j )
-            states = copy.deepcopy(place_queen(aux_states, size, index, j))
+            print("am primit pozitia ", index, j)
+            states = copy.deepcopy(place_queen(states, size, index, j))
             index += 1
             print("noul meu states: ")
-            print_states(states,size)
+            print_states(states, size)
 
     if index == 0:
         return False, aux_states
@@ -117,7 +132,6 @@ if __name__ == '__main__':
         block_i, block_j = input("Enter 'line column' to constrain").split(' ')
         block(board, int(block_i), int(block_j))
     states = []
-    # initial state
     for i in range(size):
         states.append(board[i])
 
