6320d67a3239bd9f9d8214b5a0dad58720002d4d
Leagan-Dan <leaganndanut@gmail.com>
2022-12-08 12:36:23 +0200

    .

--- Laborator_10/main.py
+++ Laborator_10/main.py
@@ -1,2 +1,25 @@
+import pprint
+
+from rdflib import Graph
+import nltk
+from nltk.corpus import treebank
+
+def test_nlth():
+    sentence = """At eight o'clock on Thursday morning
+    ... Arthur didn't feel very good."""
+    tokens = nltk.word_tokenize(sentence)
+    tagged = nltk.pos_tag(tokens)
+    nltk.chunk.ne_chunk(tagged)
+    t = treebank.parsed_sents('wsj_0001.mrg')[0]
+    t.draw()
+
+def print_rdf():
+    g = Graph()
+    g.parse("food.rdf")
+    for stmt in g:
+        pprint.pprint(stmt)
+
+
 if __name__ == '__main__':
-    print("Hello!")
+    print_rdf()
+    test_nlth()
