56e621004c4d274cf8b8e3f89469e5e3e0b5f7ec
Andrie Tablan <andreitablan01@gmail.com>
2022-12-14 20:17:19 +0200

    .

--- Laborator_10/main.py
+++ Laborator_10/main.py
@@ -1,25 +1,30 @@
 import pprint
 
+import rdflib.term
 from rdflib import Graph
 import nltk
 from nltk.corpus import wordnet as wn
 
-def test_nlth():
-    #nltk.download('omw-1.4')
-    #nltk.download('wordnet')
+
+def ok_nlth():
+    # nltk.download('omw-1.4')
+    # nltk.download('wordnet')
     wn.synsets('dog')
     wn.synsets('dog', pos=wn.VERB)
     wn.synset('dog.n.01')
     print(wn.synset('dog.n.01').definition())
     print(wn.synset('dog.n.01').examples()[0])
 
+
 def print_rdf():
     g = Graph()
     g.parse("food.rdf")
     for stmt in g:
-        pprint.pprint(stmt)
+        for index in range(0,len(stmt)):
+            if str(type(stmt[index])) != '<class \'rdflib.term.BNode\'>':
+                print(stmt[index])
+        print("-----------------------AAA---------------")
 
 
 if __name__ == '__main__':
     print_rdf()
-    test_nlth()
